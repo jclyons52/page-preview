630b4a6dd1c0dbc7e57e4d02c9473098d8010bf9
Joseph Lyons <jclyons52@gmail.com>
2016-04-09 19:50:54 +1000

    added material design card template

---
+++ src/Jclyons52/PagePreview/Templates/card.php
@@ -0,0 +1,31 @@
+<!-- Wide card with share menu button -->
+<style>
+.demo-card-wide.mdl-card {
+  width: 512px;
+}
+.demo-card-wide > .mdl-card__title {
+  color: #fff;
+  height: 176px;
+  background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.6)), url('<?= $image ?>');
+  background-size:100%;
+  color: yellow;
+
+}
+.demo-card-wide > .mdl-card__menu {
+  color: #fff;
+}
+</style>
+
+<div class="demo-card-wide mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title">
+    <h2 class="mdl-card__title-text"><?= $title ?></h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+   <?= $body ?>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="<?= $url ?>">
+      View
+    </a>
+  </div>
+</div>
